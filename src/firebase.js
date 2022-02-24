49a0fa98762ca97f909c185e98b98a0cc31e1e7a
Yash Singh Chauhan <74612612+yashsinghchauhan-formal@users.noreply.github.com>
2022-02-24 18:17:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIc5CdXeJ3mvlRePYuL6ocJVgNWAB1BmE",
-  authDomain: "clone-a79e5.firebaseapp.com",
-  projectId: "clone-a79e5",
-  storageBucket: "clone-a79e5.appspot.com",
-  messagingSenderId: "657472495871",
-  appId: "1:657472495871:web:f0b7983671d65df7ff2cca",
-  measurementId: "G-SN6DSN6TJK",
+  apiKey: "FIREBASE CONFIG",
+  authDomain: "FIREBASE CONFIG",
+  projectId: "FIREBASE CONFIG",
+  storageBucket: "FIREBASE CONFIG",
+  messagingSenderId: "FIREBASE CONFIG",
+  appId: "FIREBASE CONFIG",
+  measurementId: "FIREBASE CONFIG",
 };
 
 initializeApp(firebaseConfig);
